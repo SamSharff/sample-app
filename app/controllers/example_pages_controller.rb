99f68c6d2a783f28d36db7acfffc0bc89e0ba371
Sam Sharff <samanthaleec@gmail.com>
2022-03-07 13:15:22 -0500

    adds hello, etc. paths and methods

--- app/controllers/example_pages_controller.rb
+++ app/controllers/example_pages_controller.rb
@@ -1,2 +1,13 @@
 class ExamplePagesController < ApplicationController
+  def hello_method
+    render json: {message: "hello"}
+  end
+
+  def goodbye_method
+    render json: {message: "goodbye"}
+  end
+  
+  def purgatory_method
+    render json: {message: "you are now in purgatory"}
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,19 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html              
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  get "/hello_path", controller: "example_pages", action: "hello_method"
+
+  get "/goodbye_path", controller: "example_pages", action: "goodbye_method"
+
+  get "/purgatory_path", controller: "example_pages", action: "purgatory_method"
+
+  
 end
+
+
+# Create an api with THREE paths that return different messages!
+# Bonus: Try to render more complex information (arrays with hashes within arrays, etc.)
+# Bonus: Try to render something other than JSON! (Rails can render XML or HTML)
